8e2eed1c79e377720a868447ac5bb7e10459a7a5
Ляхов Степан <lyakhov.sr@reliab.tech>
2020-11-15 02:36:17 +0300

    refactor: refactor Badge component

--- todo_front_lab2/src/components/Badge/index.jsx
+++ todo_front_lab2/src/components/Badge/index.jsx
@@ -3,10 +3,10 @@ import classNames from 'classnames';
 
 import './Badge.scss';
 
-const Badge = ({ color, onClick, className }) => (
+const Badge = ({color, onClick, className}) => (
     <i
         onClick={onClick}
-        className={classNames('badge', { [`badge--${color}`]: color }, className)}
+        className={classNames('badge', {[`badge--${color}`]: color}, className)}
     ></i>
 );
 
